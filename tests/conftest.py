aaf4f5e52d847534577f24b0d25b96311851ffb2
leop <paukkle@gmail.com>
2022-09-16 10:26:57 +0300

    new input matrices

--- tests/conftest.py
+++ tests/conftest.py
@@ -13,6 +13,16 @@ def input_dictionary() -> dict:
 
 
 @pytest.fixture
+def input_dictionary_incorrect() -> dict:
+    items = {"K": [1, 1, 0, 0, 0, 1],
+        "I": [1, 0, 0, 2, 0],
+        "O": [0, 3, 0, 0, 1, 0],
+        "H": [0, 0, 1, 0, 2, 0],
+        "Cl": [0, 1, 1, 0, 0, 1]}
+    return items
+
+
+@pytest.fixture
 def validation_matrix():
     mat = np.array([[1, 1, 0, 0, 0, 1],
         [1, 0, 0, 2, 0, 0],
