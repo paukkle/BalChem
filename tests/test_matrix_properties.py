38e4562693abcb14cc8ee2399f9f2deaf356ef59
leop <paukkle@gmail.com>
2022-09-16 15:35:13 +0300

    augmentation matrix tests, sub functions removed

---
+++ tests/test_matrix_properties.py
@@ -0,0 +1,14 @@
+import numpy as np
+import pytest
+
+from balchem.transformations import get_augmented_matrix
+
+
+def test_create_correct_augmented_matrix(augmented_matrix):
+    augmented_matrix_output = get_augmented_matrix(augmented_matrix[:3, :])
+    assert np.array_equal(augmented_matrix, augmented_matrix_output)
+
+
+def test_create_correct_augmented_matrix_nullity_1(validation_matrix, augmented_matrix_nullity_1):
+    augmented_matrix_output = get_augmented_matrix(validation_matrix)
+    assert np.array_equal(augmented_matrix_nullity_1, augmented_matrix_output)
