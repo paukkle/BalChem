14047b658850f39c695341707d766e8208fdf035
leop <paukkle@gmail.com>
2022-09-16 13:56:31 +0300

    prints removed

--- tests/test_balance.py
+++ tests/test_balance.py
@@ -6,6 +6,4 @@ from balchem.balance import get_balance_vectors
 
 def test_correct_result(correct_balance_vector, inverted_matrix):
     balance_vectors = get_balance_vectors(inverted_matrix, 1)
-    print(f"result: {balance_vectors}")
-    print(f"test: {correct_balance_vector}")
     assert np.allclose(balance_vectors, correct_balance_vector)
