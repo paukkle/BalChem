cc0ff5437fec224b4462a56bd821774015ceec39
leop <paukkle@gmail.com>
2022-09-16 10:26:40 +0300

    Value check for input data

--- balchem/transformations.py
+++ balchem/transformations.py
@@ -1,5 +1,15 @@
+from sqlite3 import DataError
 import numpy as np
 
 
 def create_matrix(data: dict) -> np.array:
-    return np.array(list(data.values()))
+    def input_dimensions_ok(input_data: dict):
+        lengths = [len(row) for _, row in input_data.items()]
+        
+        lengths_ok = len(set(lengths)) == 1 and lengths[0] > 0
+        return lengths_ok
+
+    if input_dimensions_ok(data):
+        return np.array(list(data.values()))
+    else:
+        raise ValueError("Input row lengths are not equal to amount of columns.")
