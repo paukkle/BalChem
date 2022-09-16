c6bc4c664226f5fbdccbd01b2a4f3184be726abc
leop <paukkle@gmail.com>
2022-09-16 10:27:13 +0300

    test cases to for incorrect input data

--- tests/test_create_matrix.py
+++ tests/test_create_matrix.py
@@ -1,3 +1,4 @@
+from sqlite3 import DataError
 import numpy as np
 import pytest
 
@@ -8,3 +9,19 @@ def test_matrix_created_correctly_from_dict(input_dictionary, validation_matrix)
     mat = create_matrix(input_dictionary)
     assert np.allclose(mat, validation_matrix)
 
+
+def test_matrix_creation_fails_incorrect_input(input_dictionary_incorrect):
+    with pytest.raises(ValueError):
+        create_matrix(input_dictionary_incorrect)
+
+
+def test_matrix_creation_fails_empty_rows():
+    M = {"first": [], "second": []}
+    with pytest.raises(ValueError):
+        create_matrix(M)
+
+
+def test_matrix_creation_fails_empty_dict():
+    M = {}
+    with pytest.raises(ValueError):
+        create_matrix(M)
