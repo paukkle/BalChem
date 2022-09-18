bc55c8b883d152fe68382e3e513c3bfffc4d0e46
leop <paukkle@gmail.com>
2022-09-18 20:20:43 +0300

    new test cases

--- tests/test_balance.py
+++ tests/test_balance.py
@@ -14,3 +14,24 @@ def test_balance_equation(chemical_equation: str):
     correct_output = chemical_equation["output_equation"]
     func_output = balance_equation(input_eq)
     assert correct_output == func_output
+
+
+def test_balance_equation_methane(chemical_equation_methane: str):
+    input_eq = chemical_equation_methane["input_equation"]
+    correct_output = chemical_equation_methane["output_equation"]
+    func_output = balance_equation(input_eq)
+    assert correct_output == func_output
+
+
+def test_balance_equation_primary_alcohol(chemical_equation_primary_alcohol: str):
+    input_eq = chemical_equation_primary_alcohol["input_equation"]
+    correct_output = chemical_equation_primary_alcohol["output_equation"]
+    func_output = balance_equation(input_eq)
+    assert correct_output == func_output
+
+
+def test_balance_equation_iron_oxide(chemical_equation_iron_oxide: str):
+    input_eq = chemical_equation_iron_oxide["input_equation"]
+    correct_output = chemical_equation_iron_oxide["output_equation"]
+    func_output = balance_equation(input_eq)
+    assert correct_output == func_output
