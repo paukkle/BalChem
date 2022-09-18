5135a23ce3288e68f8042a07733f55eac3e0fd40
leop <paukkle@gmail.com>
2022-09-18 11:31:50 +0300

    balance equation string correctly created from constituents

--- balchem/balance.py
+++ balchem/balance.py
@@ -1,6 +1,33 @@
 import numpy as np
 
 
+def balance_equation(equation: str):
+    constants = (-6, -1, -6, 3, 3, 7)
+    molecules = ("KI", "KClO3", "HCl", "I2", "H2O", "KCl")
+    result = _create_balanced_equation(constants, molecules)
+    return  result
+
+
+def _create_balanced_equation(constants: tuple, molecules: tuple):
+    sides = _arrange_sides(constants, molecules)
+    full_equation = _join_sides(sides)
+    return full_equation
+
+
+def _arrange_sides(constants: tuple, molecules: tuple) -> dict:
+    sides = {"left": [], "right": []}
+    for constant, molecule in zip(constants, molecules):
+        side = "left" if constant < 0 else "right"
+        constant = abs(constant)
+        molecule_string = f"{constant} {molecule}" if constant != 1 else f"{molecule}"
+        sides[side].append(molecule_string)
+    return sides
+
+def _join_sides(sides: dict):
+    left_hand_side = " + ".join(sides["left"])
+    right_hand_side = " + ".join(sides["right"])
+    return " = ".join((left_hand_side, right_hand_side)) 
+
 def get_balance_vectors(inverted_matrix: np.array, nullity_number: int):
     null_space_vectors = _get_null_space_vectors(inverted_matrix, nullity_number)
     balance_arrays = _balance_vectors(null_space_vectors)
