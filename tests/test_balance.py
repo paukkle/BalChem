551344b9d9549c877526505296bb01c791b65c33
leop <paukkle@gmail.com>
2022-09-18 11:32:06 +0300

    balance_eqaution tests

--- tests/test_balance.py
+++ tests/test_balance.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pytest
 
-from balchem.balance import get_balance_vectors
+from balchem.balance import get_balance_vectors, balance_equation
 
 
 def test_correct_result_balance_vectors(correct_balance_vector, inverted_matrix):
@@ -9,4 +9,8 @@ def test_correct_result_balance_vectors(correct_balance_vector, inverted_matrix)
     assert np.allclose(balance_vectors, correct_balance_vector)
 
 
-
+def test_balance_equation(chemical_equation: str):
+    input_eq = chemical_equation["input_equation"]
+    correct_output = chemical_equation["output_equation"]
+    func_output = balance_equation(input_eq)
+    assert correct_output == func_output
