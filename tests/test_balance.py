0b705552c911f1f1b873f11be4b250b0203e70a3
leop <paukkle@gmail.com>
2022-09-18 10:38:44 +0300

    renamed

--- tests/test_balance.py
+++ tests/test_balance.py
@@ -4,6 +4,9 @@ import pytest
 from balchem.balance import get_balance_vectors
 
 
-def test_correct_result(correct_balance_vector, inverted_matrix):
+def test_correct_result_balance_vectors(correct_balance_vector, inverted_matrix):
     balance_vectors = get_balance_vectors(inverted_matrix, 1)
     assert np.allclose(balance_vectors, correct_balance_vector)
+
+
+
